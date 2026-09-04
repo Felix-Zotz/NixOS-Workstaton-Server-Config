11d43a9753b13fad1b251165188a9c716da524f6
Felix Zotz <felix.zotz@gmail.com>
2026-09-04 16:53:01 +0200

    refactor(vscode): improve user settings and consolidate common extensions

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -1,70 +1,75 @@
 { pkgs, ... }:
 
+let
+  rgPath = "${pkgs.ripgrep}/bin/rg";
+
+  commonSettings = {
+    "git.autofetch" = true;
+    "git.confirmSync" = false;
+    "diffEditor.codeLens" = true;
+
+    "todo-tree.ripgrep.ripgrep" = rgPath;
+
+    "workbench.colorTheme" = "Dark+";
+    "workbench.iconTheme" = "vs-seti";
+    "workbench.activityBar.location" = "top";
+    "editor.minimap.autohide" = "mouseover";
+    "telemetry.telemetryLevel" = "off";
+  };
+
+  commonExtensions = with pkgs.vscode-extensions; [
+    streetsidesoftware.code-spell-checker
+    tamasfe.even-better-toml
+    github.vscode-pull-request-github
+    davidanson.vscode-markdownlint
+    gruntfuggly.todo-tree
+  ];
+in
 {
   programs.vscode = {
     enable = true;
 
     profiles = {
-      Default = {
+      default = {
         enableExtensionUpdateCheck = false;
         enableUpdateCheck = false;
 
-        extensions = with pkgs.vscode-extensions; [
-          streetsidesoftware.code-spell-checker
-          tamasfe.even-better-toml
-          github.vscode-pull-request-github
-          davidanson.vscode-markdownlint
-          gruntfuggly.todo-tree
+        extensions = commonExtensions ++ (with pkgs.vscode-extensions; [
+          esbenp.prettier-vscode
           tomoki1207.pdf
           spadin.zmk-tools
-        ];
+        ]);
 
-        userSettings = {
+        userSettings = commonSettings // {
           "files.autoSave" = "afterDelay";
           "files.trimTrailingWhitespace" = true;
           "files.insertFinalNewline" = true;
 
           "editor.formatOnSave" = true;
-
           "editor.defaultFormatter" = "esbenp.prettier-vscode";
+
           "[markdown]" = {
-              "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
+            "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
           };
           "[toml]" = {
-              "editor.defaultFormatter" = "tamasfe.even-better-toml";
+            "editor.defaultFormatter" = "tamasfe.even-better-toml";
           };
-
-          "git.autofetch" = true;
-          "git.confirmSync" = false;
-          "diffEditor.codeLens" = true;
-
-          "todo-tree.ripgrep.ripgrep" = "/usr/bin/rg";
-
-          "workbench.colorTheme" = "Dark+";
-          "workbench.iconTheme" = "vs-seti";
-          "workbench.activityBar.location" = "top";
-          "editor.minimap.autohide" = "mouseover";
-          "telemetry.telemetryLevel" = "off";
         };
       };
 
       APA = {
-        extensions = with pkgs.vscode-extensions; [
-          streetsidesoftware.code-spell-checker
+        extensions = commonExtensions ++ (with pkgs.vscode-extensions; [
           vadimcn.vscode-lldb
           connor4312.esbuild-problem-matchers
           dbaeumer.vscode-eslint
-          tamasfe.even-better-toml
           ms-vscode.extension-test-runner
-          github.vscode-pull-request-github
-          davidanson.vscode-markdownlint
           christian-kohler.npm-intellisense
           esbenp.prettier-vscode
           yoavbls.pretty-ts-errors
           rust-lang.rust-analyzer
-          gruntfuggly.todo-tree
-        ];
-        userSettings = {
+        ]);
+
+        userSettings = commonSettings // {
           "files.autoSave" = "afterDelay";
           "files.trimTrailingWhitespace" = true;
           "files.insertFinalNewline" = true;
@@ -72,60 +77,31 @@
           "editor.formatOnSave" = true;
 
           "[markdown]" = {
-              "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
+            "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
           };
           "[toml]" = {
-              "editor.defaultFormatter" = "tamasfe.even-better-toml";
+            "editor.defaultFormatter" = "tamasfe.even-better-toml";
           };
-
-          "git.autofetch" = true;
-          "git.confirmSync" = false;
-          "diffEditor.codeLens" = true;
-
-          "todo-tree.ripgrep.ripgrep" = "/usr/bin/rg";
-
-          "workbench.colorTheme" = "Dark+";
-          "workbench.iconTheme" = "vs-seti";
-          "workbench.activityBar.location" = "top";
-          "editor.minimap.autohide" = "mouseover";
-          "telemetry.telemetryLevel" = "off";
         };
       };
 
       FLL = {
-        extensions = with pkgs.vscode-extensions; [
-          streetsidesoftware.code-spell-checker
-          tamasfe.even-better-toml
-          github.vscode-pull-request-github
-          davidanson.vscode-markdownlint
+        extensions = commonExtensions ++ (with pkgs.vscode-extensions; [
           ms-python.python
           charliermarsh.ruff
-          gruntfuggly.todo-tree
-        ];
-        userSettings = {
-          "git.autofetch" = true;
-          "git.confirmSync" = false;
-          "diffEditor.codeLens" = true;
+        ]);
 
-          "todo-tree.ripgrep.ripgrep" = "/usr/bin/rg";
-
-          "workbench.colorTheme" = "Dark+";
-          "workbench.iconTheme" = "vs-seti";
-          "workbench.activityBar.location" = "top";
-          "editor.minimap.autohide" = "mouseover";
-          "telemetry.telemetryLevel" = "off";
-        };
+        userSettings = commonSettings;
       };
 
       NixOS = {
         extensions = with pkgs.vscode-extensions; [
           davidanson.vscode-markdownlint
           gruntfuggly.todo-tree
-          kamadorueda.alejandra
           jnoortheen.nix-ide
         ];
 
-        userSettings = {
+        userSettings = commonSettings // {
           "nix.enableLanguageServer" = true;
           "nix.serverPath" = "${pkgs.nil}/bin/nil";
           "nix.serverSettings.nil.formatting.command" = [
@@ -136,57 +112,29 @@
             "editor.defaultFormatter" = "jnoortheen.nix-ide";
             "editor.formatOnSave" = true;
           };
-          "git.autofetch" = true;
-          "git.confirmSync" = false;
-          "diffEditor.codeLens" = true;
-
-          "todo-tree.ripgrep.ripgrep" = "/usr/bin/rg";
-
-          "workbench.colorTheme" = "Dark+";
-          "workbench.iconTheme" = "vs-seti";
-          "workbench.activityBar.location" = "top";
-          "editor.minimap.autohide" = "mouseover";
-          "telemetry.telemetryLevel" = "off";
         };
       };
 
       WEB = {
-        extensions = with pkgs.vscode-extensions; [
+        extensions = commonExtensions ++ (with pkgs.vscode-extensions; [
           formulahendry.auto-rename-tag
           biomejs.biome
-          streetsidesoftware.code-spell-checker
-          github.vscode-pull-request-github
           ecmel.vscode-html-css
-          davidanson.vscode-markdownlint
-          gruntfuggly.todo-tree
           tomoki1207.pdf
-        ];
+        ]);
 
-        userSettings = {
+        userSettings = commonSettings // {
           "editor.formatOnSave" = true;
-          "git.autofetch" = true;
-          "git.confirmSync" = false;
-          "diffEditor.codeLens" = true;
-
           "editor.defaultFormatter" = "biomejs.biome";
+
           "[markdown]" = {
             "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
           };
           "[toml]" = {
             "editor.defaultFormatter" = "tamasfe.even-better-toml";
           };
-
-          "todo-tree.ripgrep.ripgrep" = "/usr/bin/rg";
-
-          "workbench.colorTheme" = "Dark+";
-          "workbench.iconTheme" = "vs-seti";
-          "workbench.activityBar.location" = "top";
-          "editor.minimap.autohide" = "mouseover";
-          "telemetry.telemetryLevel" = "off";
         };
       };
     };
   };
 }
-
-
